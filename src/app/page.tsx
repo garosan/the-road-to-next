6f54059b528a4ee07a86b8f512c6af24f8d8366a
Garosan <garosanchez27@gmail.com>
2025-05-07 08:16:37 -0600

    Add tickets page, linking, dummy ticket data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import Link from "next/link";
+import { ticketsPath } from "@/paths";
+
+const HomePage = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello World!</h1>
+    <div>
+      <h2 className="text-xl">HomePage</h2>
+
+      <Link href={ticketsPath()} className="underline">
+        Go To Tickets
+      </Link>
     </div>
   );
-}
+};
+
+export default HomePage;
