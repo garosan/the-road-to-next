833747240ad369570c1671e6e072a1d895c8285d
Garosan <garosanchez27@gmail.com>
2025-05-07 10:05:24 -0600

    Begin to add Tailwind classes and conditional styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-xl">HomePage</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
 
-      <Link href={ticketsPath()} className="underline">
-        Go To Tickets
-      </Link>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go To Tickets
+        </Link>
+      </div>
     </div>
   );
 };
